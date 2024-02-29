2c4b63df240ad0a4f51a6450f286c081a38cf087
andreasgiannoudis <agiannoudis@hotmail.com>
2024-02-29 20:21:58 +0100

    The cart page is ready. I created a custom may like products shortcode. I added the functionality for the free shipping to be enabled automatically when the order amount is over the free shipping amount.

--- wp-content/themes/mytheme/resources/js/app.js
+++ wp-content/themes/mytheme/resources/js/app.js
@@ -1,3 +1,28 @@
 import "./update-cart-total-items-basket";
 import "./lazy-load-products";
 
+jQuery(document).ready(function($) {
+    $('.woocommerce').before($('.woocommerce-notices-wrapper'));
+});
+
+
+
+jQuery(document).ready(function($) {
+    function handleShippingOptions() {
+        if ($('input[name^="shipping_method"]:checked').val() === 'free_shipping:1') {
+            $('input[name^="shipping_method"]').not(':checked').closest('ul').hide();
+            $('.shipping').html('<th class="shipping" colspan="1">Shipping:</th><td class="shipping-amount" colspan="1">0kr</td>');
+        } else {
+            $('input[name^="shipping_method"]').closest('tr').show();
+            $('.shipping-total').text('');
+        }
+    }
+    handleShippingOptions();
+
+    $('input[name^="shipping_method"]').on('change', function() {
+        handleShippingOptions();
+    });
+});
+
+
+
--- wp-content/themes/mytheme/shortcodes.php
+++ wp-content/themes/mytheme/shortcodes.php
@@ -5,6 +5,7 @@ require_once("parts/custom_product_photo.php");
 require_once("parts/custom_cta_sale.php");
 require_once("parts/custom_top_rated_products.php");
 require_once("parts/custom_newsletter.php");
+require_once("parts/may_like_products.php");
 
 
 add_shortcode('display_advantages', 'display_advantages_shortcode');
@@ -12,5 +13,6 @@ add_shortcode('custom_product_photo', 'mytheme_custom_product_photo');
 add_shortcode('custom_cta_sale', 'mytheme_custom_cta_sale');
 add_shortcode('top_rated_products', 'display_top_rated_products_shortcode');
 add_shortcode('newsletter_subscription_form', 'newsletter_subscription_form_shortcode');
+add_shortcode( 'also_may_buy', 'display_also_may_buy_products' );
 
 
